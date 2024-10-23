3f780cd2fb370a677df4e63b1489921749392f20
nicholas <nickcts@hotmail.com>
2024-10-23 12:16:45 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -44,10 +42,41 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+
+        # Update the plant's fields if they are provided in the request
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        # You can add more fields here if needed to update other parts of the plant model
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)  # Return 204 No Content
 
 
+# Make sure the updated class is used for the '/plants/<int:id>' route
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
